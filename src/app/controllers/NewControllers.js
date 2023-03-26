2953666d29cc522980585c95415733204a676b93
Tran Phuoc Thien <thientran01345@icloud.com>
2023-03-26 23:19:28 +0700

    Test Husky

--- src/app/controllers/NewControllers.js
+++ src/app/controllers/NewControllers.js
@@ -1,12 +1,12 @@
 class NewControllers {
-  // [GET] /news
-  index(req, res) {
-    res.render("news");
-  }
-  // [GET] /:slug
-  show(req, res) {
-    res.send("PHIÊN BẢN NEWS MỚI CẬP NHẬT");
-  }
+    // [GET] /news
+    index(req, res) {
+        res.render('news');
+    }
+    // [GET] /:slug
+    show(req, res) {
+        res.send('PHIÊN BẢN NEWS MỚI CẬP NHẬT');
+    }
 }
 
 module.exports = new NewControllers();
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const morgan = require("morgan");
-const hbs = require("express-handlebars");
+const express = require('express');
+const morgan = require('morgan');
+const hbs = require('express-handlebars');
 const app = express();
 const port = 3000;
-const path = require("path");
-const route = require("./routes");
+const path = require('path');
+const route = require('./routes');
 
 // Lấy ảnh (img/logo.png)
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 // Middleware xử lý từ form submit
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 
@@ -20,17 +20,17 @@ app.use(express.json());
 // app.use(morgan("combined"));
 // Template engine
 app.engine(
-  "hbs",
-  hbs.engine({
-    extname: ".hbs",
-  })
+    'hbs',
+    hbs.engine({
+        extname: '.hbs',
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Router init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
