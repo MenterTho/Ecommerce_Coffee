c532dab0ac809588015429be6d0cc068bec441b4
Tran Phuoc Thien <thientran01345@icloud.com>
2023-04-07 00:20:07 +0700

    Them slide cho home, fix loi hinh anh user

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const User = new Schema(
-  {
-    username: { type: String },
-    password: { type: String },
-    email: { type: String },
-    image: { type: String },
-    access: { type: String },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        username: { type: String },
+        password: { type: String },
+        email: { type: String },
+        image: { type: String, default: 'img/user.png' },
+        access: { type: String },
+    },
+    {
+        timestamps: true,
+    },
 );
 
-module.exports = mongoose.model("User", User);
+module.exports = mongoose.model('User', User);
---
+++ src/public/js/main.js
@@ -0,0 +1,13 @@
+//Swiper slider
+var swiper = new Swiper('.bg-slider-thumbs', {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
+});
+var swiper2 = new Swiper('.bg-slider', {
+    loop: true,
+    spaceBetween: 0,
+    thumbs: {
+        swiper: swiper,
+    },
+});
